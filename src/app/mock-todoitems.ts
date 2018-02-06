ef415512ed94cb4e1b3c5c8935f304fd025471ba
sfahadahmed <sfahadahmed@gmail.com>
2018-02-06 10:34:47 +0500

    Adding support for todo item class and some mock data for testing.

---
+++ src/app/mock-todoitems.ts
@@ -0,0 +1,8 @@
+import { TodoItem } from './todoitem'
+
+export const TODOITEMS : TodoItem[] = [
+    {id: 1, title: 'Daily Meeting', description: 'We have a daily meeting at 10:AM'},
+    {id: 2, title: 'Finish Report', description: 'Finish up the customer report today.'},
+    {id: 3, title: 'Grocery', description: 'Go to the grocery shopping.'},
+    {id: 4, title: 'Book Reading', description: 'Read the book before going to bed.'}
+];
