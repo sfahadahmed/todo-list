b67873e521552dd896448cc8625e3739bf7b443b
sfahadahmed <sfahadahmed@gmail.com>
2018-02-08 22:15:14 +0500

    Detail View - Adding support to extract ID from the route path and fetch a single item from the service - Adding back button to move to the previous screen

--- src/app/todolist-detail/todolist-detail.component.ts
+++ src/app/todolist-detail/todolist-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { TodoItem } from '../todoitem'
+import { TodolistService } from '../todolist.service';
 
 @Component({
   selector: 'app-todolist-detail',
@@ -10,9 +14,19 @@ export class TodolistDetailComponent implements OnInit {
 
   @Input() todoItem: TodoItem;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private todoListService: TodolistService) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.todoListService.getDataById(id).
+      subscribe(todoItem => this.todoItem = todoItem);
+  }
+
+  back(): void {
+    this.location.back();
+  }
 }
